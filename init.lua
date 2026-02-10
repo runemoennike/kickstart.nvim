3b9310dd0ef97a40bebf845670084c69f5dfe614
Rune A. Juel Mønnike <rajm@force.dk>
2026-02-10 14:11:28 +0100

    TS requires.

--- init.lua
+++ init.lua
@@ -904,7 +904,6 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers.mason or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'gitcommit',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -1169,7 +1168,26 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'c_sharp',
+        'diff',
+        'javascript',
+        'gitcommit',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'powershell',
+        'python',
+        'query',
+        'toml',
+        'vim',
+        'vimdoc',
+        'yaml',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
