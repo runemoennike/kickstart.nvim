35b04db605b1e8bd89aa663c2605fc80582e58d7
Rune A. Juel Mønnike <rajm@force.dk>
2026-03-06 12:35:30 +0100

    Neovide config.

--- init.lua
+++ init.lua
@@ -134,10 +134,7 @@ vim.keymap.set('n', '<leader>c', function()
 end, { desc = 'Open [C]onsole' })
 
 if vim.g.neovide == true then
-  -- vim.cmd 'set guifont=Hack\ NF:h10'
-  -- vim.o.guifont='Consolas:h10'
-  -- vim.o.guifont='FiraCode NF:h14'
-  vim.api.nvim_set_keymap('n', '<F11>', ':let g:neovide_fullscreen = !g:neovide_fullscreen<CR>', {})
+  require 'neovide'
 end
 
 -- [[ Basic Autocommands ]]
---
+++ neovide.lua
@@ -0,0 +1,13 @@
+-- vim.cmd 'set guifont=Hack\ NF:h10'
+-- vim.o.guifont='Consolas:h10'
+vim.o.guifont = 'FiraCode Nerd Font:h8'
+vim.api.nvim_set_keymap('n', '<F11>', ':let g:neovide_fullscreen = !g:neovide_fullscreen<CR>', {})
+
+vim.keymap.set({ 'n', 'v' }, '<C-+>', ':lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>')
+vim.keymap.set({ 'n', 'v' }, '<C-->', ':lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>')
+vim.keymap.set({ 'n', 'v' }, '<C-0>', ':lua vim.g.neovide_scale_factor = 1<CR>')
+
+vim.keymap.set({ 'n', 'v' }, '<C-ScrollWheelUp>', ':lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>')
+vim.keymap.set({ 'n', 'v' }, '<C-ScrollWheelDown>', ':lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>')
+
+vim.g.neovide_cursor_animation_length = 0
