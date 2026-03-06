81b4c4c13b302795a8c5c6456711e058745181e6
Rune A. Juel Mønnike <rajm@force.dk>
2026-03-06 12:41:02 +0100

    Font size 10.

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,6 +1,6 @@
 -- vim.cmd 'set guifont=Hack\ NF:h10'
 -- vim.o.guifont='Consolas:h10'
-vim.o.guifont = 'FiraCode Nerd Font:h8'
+vim.o.guifont = 'FiraCode Nerd Font:h10'
 vim.api.nvim_set_keymap('n', '<F11>', ':let g:neovide_fullscreen = !g:neovide_fullscreen<CR>', {})
 
 vim.keymap.set({ 'n', 'v' }, '<C-+>', ':lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>')
